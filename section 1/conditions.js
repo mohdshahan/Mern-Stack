c1d5778573b15311d65ce7db78ce7e5fc1c22692
mohdshahan <mohdshahan171@gmail.com>
2024-07-21 20:32:20 +0530

    js

---
+++ section 1/conditions.js
@@ -0,0 +1,11 @@
+
+if(n%2 === 0){
+    a = 'nice';
+    let x ='somethoing';
+console.log('even');
+}else{
+    console.log('odd');
+}
+
+console.log(a);
+console.log(x);
---
+++ section 1/index.js
@@ -0,0 +1,10 @@
+console.log('hello javascript');
+console.log('Nice');
+
+num= 7234;
+myname='shahan';
+console.log('myname');
+
+console.log('typeof num')
+console.log('typeof myname')
+
